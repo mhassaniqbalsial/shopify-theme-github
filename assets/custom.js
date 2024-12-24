d5a0762a6ffc0bdd903cbae9fa7b5c412f8cf412
mhassaniqbalsial <m.hassaniqbalsial@gmail.com>
2024-12-24 15:46:24 +0500

    innitialization

---
+++ assets/custom.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.getElementsByTagName("body")[0];
+  // or you could use: const body = document.body;
+  body.style.backgroundColor = "red!important";
+});
